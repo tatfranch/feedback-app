28992cf7fbfcfdefca86ff0a58de3d089da10486
Tatiana Paga Franch <tatianapagafranch@gmail.com>
2022-03-01 19:49:33 +0100

    setting up state hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/Header';
+import FeedbackItem from './components/FeedbackItem';
 
 function App() {
   return (
     <>
       <Header />
       <div className='container'>
-        <h1>My App</h1>
+        <FeedbackItem />
       </div>
     </>
   );
