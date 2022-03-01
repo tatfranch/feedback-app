92b119cdaf970d1ea938a9f81d094940d9455bd0
Tatiana Paga Franch <tatianapagafranch@gmail.com>
2022-03-01 18:35:32 +0100

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+function Header({ text }) {
+  return (
+    <header>
+      <div className='container'>
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  text: 'Feedback UI',
+};
+
+Header.propTypes = {
+  text: PropTypes.string,
+};
+export default Header;
