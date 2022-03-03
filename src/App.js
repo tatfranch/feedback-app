d14911b6b1a354004a83ad4f2c8a691daa41c31f
Tatiana Paga Franch <tatianapagafranch@gmail.com>
2022-03-03 14:35:03 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import FeedbackData from './data/FeedbackData';
+import AboutPage from './components/pages/AboutPage';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,14 +23,29 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ src/components/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+import Card from '../shared/Card';
+function AboutPage() {
+  return (
+    <Card>
+      <div className='about'>
+        <h1>About This Project</h1>
+        <p>This is a React App to leave feedback for a product or service</p>
+        <p>Version: 1.0.0</p>
+
+        <p>
+          <a href='/'>Back to Home</a>
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
