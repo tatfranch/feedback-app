1adf17c2a690d61c54e947e27101dc29b5949caa
Tatiana Paga Franch <tatianapagafranch@gmail.com>
2022-03-21 10:25:54 +0100

    cors update

--- server/index.js
+++ server/index.js
@@ -6,17 +6,13 @@ const FeedbackModel = require('./models/Feedback');
 require('dotenv').config();
 
 const cors = require('cors'); //used to connect with react
-// const corsOptions = {
-//   origin: '*',
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(
   cors({
     origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
   })
 );
 
